1f0509527966d9e592fad9684ff4f263b055eb56
andreiibl <andrei.balasoiu03@gmail.com>
2025-01-23 15:58:06 +0100

    Funcionalidad añadida.

--- Phoneword/MainPage.xaml.cs
+++ Phoneword/MainPage.xaml.cs
@@ -2,24 +2,52 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        string numeroTraducido;
+
+        private void OnTraducir(object sender, EventArgs e)
         {
-            count+=5;
+            string numeroIntroducido = PhoneNumberText.Text;
+            numeroTraducido = Core.PhonewordTranslator.ToNumber(numeroIntroducido);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+            if (!string.IsNullOrEmpty(numeroTraducido))
+            {
+                BotonLlamar.IsEnabled = true;
+                BotonLlamar.Text = "Llamar " + numeroTraducido;
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                BotonLlamar.IsEnabled = false;
+                BotonLlamar.Text = "Llamar";
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        async void OnLlamar(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Llamar al numero",
+                "¿Te gustaría llamar al numero " + numeroTraducido+ "?",
+                "Sí",
+                "No")) 
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(numeroTraducido);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("No se a podido llamar", "El numero de teléfono no es válido.", "Ok");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("No se a podido llamar", "Ha ocurrido un error.", "Ok");
+                }
+            }
         }
     }
-
 }
